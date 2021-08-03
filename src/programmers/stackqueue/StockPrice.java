43734232f244b4c299d8763256d9b52b3e4f70d5
jyjang <jyjang@enliple.com>
2021-08-03 18:06:01 +0900

    [코딩테스트 연습 > 스택/큐 > 프린터] 배열 사용해서 해결.

---
+++ src/programmers/stackqueue/StockPrice.java
@@ -0,0 +1,29 @@
+package programmers.stackqueue;
+
+import java.util.Arrays;
+
+/**
+ * 문제 설명
+ * 초 단위로 기록된 주식가격이 담긴 배열 prices가 매개변수로 주어질 때, 가격이 떨어지지 않은 기간은 몇 초인지를 return 하도록 solution 함수를 완성하세요.
+ *
+ * 제한사항
+ * prices의 각 가격은 1 이상 10,000 이하인 자연수입니다.
+ * prices의 길이는 2 이상 100,000 이하입니다.
+ */
+public class StockPrice {
+    public static void main(String[] args){
+        int[] prices = {1, 2, 3, 2, 3};
+        int[] result = solution(prices);
+        System.out.println(Arrays.toString(result));//[4, 3, 1, 1, 0]
+    }
+    public static int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for(int i=0; i<prices.length; i++){
+            for(int j=i+1; j<prices.length;j++){
+                answer[i]++;
+                if(prices[i]>prices[j]) break;
+            }
+        }
+        return answer;
+    }
+}
